5acbde136caf7ceb574f6be9879e21d56ab70cfe
Rochelle Markham <rmarkham@my365.bellevue.edu>
2020-07-15 15:35:10 -0500

    Week-2

---
+++ api-gateway/models/user.js
@@ -0,0 +1,10 @@
+//fields usersname, password and email
+var mongoose = require ('mongoose');
+var userSchema = new mongoose.Schema({
+    username: String, 
+    password: String,
+    email: String
+});
+module.exports = mongoose.model('User', userSchema);
+
+//database queries
